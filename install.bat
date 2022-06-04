1b1dd742274b9e3ef2ed92d9c8bf90d91d230847
Moorea Tv <moorea@ymail.com>
2022-06-04 10:10:20 -0700

    Classic era is now 11403 and SL is 90205

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-for %%x in (_retail_ _classic_ _ptr_) do (
+for %%x in (_retail_ _classic_ _classic_era_) do (
 echo Installing for %%x
 xcopy /i /y AuctionDB\*.* "C:\Program Files (x86)\World of Warcraft\%%x\Interface\Addons\AuctionDB"
 rem xcopy /i /y AuctionDB\locale\*.* "C:\Program Files (x86)\World of Warcraft\%%x\Interface\Addons\AuctionDB\locale"
